1d812eb6c091d732fcfaf7e3699b614b00e60e75
MehulVerma17 <158771424+MehulVerma17@users.noreply.github.com>
2024-03-15 18:35:08 +0530

    Time: 35 ms (89.14%) | Memory: 16.9 MB (71.42%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -4,4 +4,18 @@ class Solution:
             return True 
         else:
             return False
-       
+        '''
+        #less optimal
+        occur=0
+        d={}
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if arr[i]==arr[j]:
+                    occur+=1
+            d[arr[i]]=occur
+            occur=0
+        if len(d)==len(set(d.values())):
+            return True
+        else:
+            return False
+        '''
