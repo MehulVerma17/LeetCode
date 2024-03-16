ba48a4f18a82116484eabc717a7bcb5c36f2a633
MehulVerma17 <158771424+MehulVerma17@users.noreply.github.com>
2024-03-16 21:58:22 +0530

    Time: 233 ms (17.25%) | Memory: 18.2 MB (64.45%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        l=[]
+    
+        for i in range(len(s)):
+            if s[i].isalpha():
+                l.append(s[i])
+            elif s[i]=="*":
+                l.pop()
+            
+        return "".join(l)
+        
