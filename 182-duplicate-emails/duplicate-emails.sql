ed2897656dd2e75ae17e23644bff7354034a465b
Mehul Verma <158771424+MehulVerma17@users.noreply.github.com>
2024-06-11 19:15:08 +0530

    Time: 315 ms (99.33%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT DISTINCT P1.Email FROM Person P1,Person P2 WHERE P1.id <> P2.id AND P1.Email=P2.Email
+SELECT DISTINCT P1.Email from Person P1,Person P2 where P1.id <> P2.id and P1.Email=P2.Email
